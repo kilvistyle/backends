2109eb1f32cd57a4c9928437fa0a2e842bf77979
satoshi chiba <t-satchiba@yahoo-corp.jp>
2019-07-04 12:25:45 +0900

    del comment

--- src/main/java/com/example/backends/entity/Body.java
+++ src/main/java/com/example/backends/entity/Body.java
@@ -5,16 +5,6 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-/**
- * <p>Body</p>
- * <p>TODO クラスコメント</p>
- * <p>
- * ・新規作成 2019/06/23 S.Chiba.<br>
- * </p>
- *
- * @author S.Chiba
- * @since 2019/06/23
- */
 @Data
 @Builder
 public class Body implements Serializable {
--- src/main/java/com/example/backends/entity/Comment.java
+++ src/main/java/com/example/backends/entity/Comment.java
@@ -6,16 +6,6 @@ import lombok.Data;
 import java.io.Serializable;
 import java.util.Date;
 
-/**
- * <p>Comment</p>
- * <p>TODO クラスコメント</p>
- * <p>
- * ・新規作成 2019/06/23 S.Chiba.<br>
- * </p>
- *
- * @author S.Chiba
- * @since 2019/06/23
- */
 @Data
 @Builder
 public class Comment implements Serializable {
